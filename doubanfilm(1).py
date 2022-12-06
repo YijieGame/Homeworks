9529efd2c3a3a21d742e41013e237f3c68a2abc2
111huan <1901464550@qq.com>
2022-12-06 20:22:33 +0800

    Update doubanfilm(1).py

--- doubanfilm(1).py
+++ doubanfilm(1).py
@@ -8,9 +8,9 @@ import re
 # csv文件：数据与数据间用逗号隔开，方便进行数据处理
 fp = open("top250.text", mode="w", encoding='utf-8')
 
-# url = "https://movie.douban.com/top250"
+# url = "https://www.jjwxc.net/topten.php?orderstr=3&t=1"
 for num in range(0, 250, 25):
-    url = "https://movie.douban.com/top250?start=num&filter="
+    url = "https://www.jjwxc.net/topten.php?orderstr=3&t=1"
     # 进行反爬处理
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36"
@@ -24,28 +24,16 @@ for num in range(0, 250, 25):
 
     # 编写正则表达式
     # re.S可以让正则中的.匹配换行符
-    obj = re.compile(r'<div class="item">.*?<span class="title">(?P<filename>.*?)</span>'
-                     r'.*?<p class="">.*?导演: (?P<director>.*?)&nbsp;.*?<br>'
-                     r'(?P<year>.*?)&nbsp;.*?&nbsp;(?P<country>.*?)&nbsp;'
-                     r'.*?<span class="rating_num" property="v:average">'
-                     r'(?P<score>.*?)</span>.*?<span>(?P<number>.*?)人评价</span>', re.S)
-
     # 进行正则匹配
-    result = obj.finditer(pageSource)
-    for item in result:
-        filename = item.group("filename")
-        director = item.group("director")
-        year = item.group("year").strip()  # 去除字符串左右两端的空白
-        country = item.group("country")
-        score = item.group("score")
-        number = item.group("number")
-        print("电影名称:" + filename)
-        print("导演：" + director)
-        print("年代：" + year)
-        print("地区：" + country)
-        print("豆瓣评分：" + score)
-        print("共有" + number + "人为该电影评分\n")
-        fp.write(f"{filename},{director},{year},{country},{score},{number}\n")
+    for i in (0,10):
+        obj = re.compile(r'target=\"_blank\"\nhref=\"(?P<filename>.*?)\"',
+                         r'target=\"_blank\"\nhref=\"(?P<filename>.*?)\"',re.S)
+        result = obj.finditer(pageSource)
+        filename = result[0].group("filename")
+
+
+        print("名称:" + filename)
+        fp.write(f"{filename},\n")
 
 fp.close()
 
